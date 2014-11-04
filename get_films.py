31546a4ddd12a73e718af942cfdb68386b1b686d
alex <liangbinsi@gmail.com>
2014-11-05 00:55:11 +0800

    update person

--- get_films.py
+++ get_films.py
@@ -6,12 +6,14 @@ from scrapy.crawler import Crawler
 from scrapy import log, signals
 from scrapy.utils.project import get_project_settings
 from douban.spiders.filmSpider import FilmSpider
+from douban.spiders.personSpider import PersonSpider
 
 settings = get_project_settings()
 crawler = Crawler(settings)
 crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
 crawler.configure()
-crawler.crawl(FilmSpider())
+# crawler.crawl(FilmSpider())
+crawler.crawl(PersonSpider())
 crawler.start()
 log.start()
 reactor.run()
